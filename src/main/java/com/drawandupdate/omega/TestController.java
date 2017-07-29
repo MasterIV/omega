484742be070f8cbc2a0b345fe1d0720cee9326f2
Tobias Rojahn <tobias@rojahn.org>
2017-07-28 17:59:54 -0700

    template engine example, learning: only use one

--- src/main/java/com/drawandupdate/omega/TestController.java
+++ src/main/java/com/drawandupdate/omega/TestController.java
@@ -2,8 +2,11 @@ package com.drawandupdate.omega;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Map;
 
 @Controller
 public class TestController {
@@ -12,5 +15,11 @@ public class TestController {
     @ResponseBody
     public TestPojo index() {
         return new TestPojo("hello", "world");
+    }
+
+    @RequestMapping("/foo")
+    public String templatetest(Map<String, Object> model) {
+        model.put("world", "Schinken");
+        return "test";
     }
 }
